07dc381e12480d09408f35ac8a1cdcb7acab845d
wuding <wuding129@163.com>
2017-12-28 08:51:35 +0800

    init

---
+++ src/store/index.js
@@ -0,0 +1,25 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+const debug = process.env.NODE_ENV !== 'production'
+
+export default new Vuex.Store({
+  state: {
+
+  },
+  getters: {
+
+  },
+  mutations: {
+
+
+
+  },
+  actions: {},
+  modules: {
+
+  },
+  strict: debug
+})
